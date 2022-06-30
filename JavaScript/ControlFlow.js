d707322c25a03d80139abc0c8482e355b33b0840
ericnuss509 <erictylernuss@gmail.com>
2022-06-29 22:38:44 -0700

    algo video

---
+++ JavaScript/ControlFlow.js
@@ -0,0 +1,67 @@
+// for loops
+// initializing let i = 0
+// condition i < 5 true or false
+// final expression i++
+// for(let i=0; i < 5; i++){
+// code block
+//     console.log('in loop:', i);
+
+// }
+// console.log('finished')
+
+
+
+
+
+// const names= ['eric', 'gary', 'manfred'];
+
+// for(let i=0; i< names.length; i++){
+    //this will get the position of i
+    // console.log(i);
+    //this will log all the names
+    // console.log(names[i]);
+    // let html = <div>${names[i]}</div>;
+    // console.log(html);
+// }
+
+
+
+//while loops
+// const names = ['eric', 'gary', 'manfred'];
+// let i =0;
+
+// while(i < 5){
+//     console.log('in loop:', i);
+//     i++;
+// }
+
+// let i = 0;
+// while(i < names.length){
+//     console.log(names[i]);
+//     i++;
+// }
+
+// do while loops
+
+// let i = 3;
+
+// do{
+//     console.log('val of i is:', i);
+//     i++;
+
+// }while(i<5);
+
+//if statements
+
+const age = 25;
+
+if(age > 20){
+    console.log('you are older than 20 years old.');
+}
+
+const ninja =['eric', 'gary', 'nardos'];
+
+if(ninja.length > 4){
+    console.log("lots of ninja");
+
+}
